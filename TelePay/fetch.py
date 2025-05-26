d760c769e5394a5be5ad37fdfe9e99d14e23b851
YossTechLLC <152027993+YossTechLLC@users.noreply.github.com>
2025-05-26 04:36:22 -0400

    Create fetch.py

---
+++ TelePay/fetch.py
@@ -0,0 +1,29 @@
+# fetch.py
+import os
+from google.cloud import secretmanager
+
+def fetch_telegram_token():
+    try:
+        client = secretmanager.SecretManagerServiceClient()
+        secret_name = os.getenv("TELEGRAM_BOT_SECRET_NAME")
+        if not secret_name:
+            raise ValueError("Environment variable TELEGRAM_BOT_SECRET_NAME is not set.")
+        secret_path = f"{secret_name}"
+        response = client.access_secret_version(request={"name": secret_path})
+        return response.payload.data.decode("UTF-8")
+    except Exception as e:
+        print(f"Error fetching the Telegram bot TOKEN: {e}")
+        return None
+
+def fetch_payment_provider_token():
+    try:
+        client = secretmanager.SecretManagerServiceClient()
+        secret_name = os.getenv("PAYMENT_PROVIDER_SECRET_NAME")
+        if not secret_name:
+            raise ValueError("Environment variable PAYMENT_PROVIDER_SECRET_NAME is not set.")
+        secret_path = f"{secret_name}"
+        response = client.access_secret_version(request={"name": secret_path})
+        return response.payload.data.decode("UTF-8")
+    except Exception as e:
+        print(f"Error fetching the PAYMENT_PROVIDER_TOKEN: {e}")
+        return None
