c966c3f8a704fcac4e2915bdc443931f297f1e61
YossTechLLC <yosstechllc@gmail.com>
2025-06-19 04:01:11 -0400

    donation button shot 4

--- 18/Refracted-1/bot_manager.py
+++ 18/Refracted-1/bot_manager.py
@@ -48,7 +48,7 @@ class BotManager:
         # Donation conversation handler
         donation_handler = ConversationHandler(
             entry_points=[
-                CallbackQueryHandler(self.input_handlers.start_donation, pattern="^CMD_DONATE$"),
+                CallbackQueryHandler(self.menu_callback_handler, pattern="^CMD_DONATE$"),
             ],
             states={
                 DONATION_AMOUNT_INPUT: [MessageHandler(filters.TEXT & ~filters.COMMAND, self.input_handlers.receive_donation_amount)],
